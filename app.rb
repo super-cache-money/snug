e89bae4fb43060567e68550976255107527db87a
Henrish Maluleka <hmmalul11@earlham.edu>
2016-09-17 15:44:28 +0200

    Feature: basic login

--- app.rb
+++ app.rb
@@ -1,12 +1,29 @@
 
 require 'sinatra'
+require_relative 'wongo'
 get '/frank-says' do ||
   'Put this in your pipe & smoke it!'
 end
 
 
 
-get '/:user' do |user|
-  "hello #{user}"
+# get '/:user' do |user|
+#   "hello #{user}"
+# end
+
+get '/' do
+  haml :index
 end
 
+post '/login' do
+  validateUser(params[:username], params[:password]).to_s
+end
+
+def validateUser(username, password)
+  #if password matches return user
+  if getUser(username)["password"] == password
+    getUser(username)
+  else
+    "user not found in our records"
+  end
+end
